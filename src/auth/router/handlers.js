9f0ca00e42d9761ad50f32b7813235c04396bd38
Spacecowboi <cunningham2192@gmail.com>
2023-09-26 21:43:29 -0700

    putting this down for tonight, need to go to bed, I don't know what the tests are doing anymore or what is wrong where

--- src/auth/router/handlers.js
+++ src/auth/router/handlers.js
@@ -9,39 +9,44 @@ async function handleSignup(req, res, next) {
       user: userRecord,
       token: userRecord.token
     };
-    res.status(200).json(output);
+    res.status(201).json(output);
   } catch (e) {
     console.error(e);
-    next(e);
+    res.status(500).send('Error creating a user');
   }
 }
 
 async function handleSignin(req, res, next) {
   try {
     const user = {
-      user: request.user,
-      token: request.user.token
+      user: req.user,
+      token: req.user.token
     };
     res.status(200).json(user);
   } catch (e) {
     console.error(e);
-    next(e);
+    res.status(500).send('Error signing in as user');
   }
 }
 
 async function handleGetUsers(req, res, next) {
   try {
-    const userRecords = await Users.findAll({});
-    const list = users.map(user => user.username);
+    const userRecords = await users.findAll({});
+    const list = userRecords.map(user => user.username);
     res.status(200).json(list);
   } catch (e) {
     console.error(e);
-    next(e);
+    res.status(500).send('Error getting users');
   }
 }
 
 function handleSecret(req, res, next) {
-  res.status(200).text("Welcome to the secret area!");
+  try {
+    res.status(200).send("Welcome to the secret area!");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Error accessing secret area');
+  }
 }
 
 module.exports = {
